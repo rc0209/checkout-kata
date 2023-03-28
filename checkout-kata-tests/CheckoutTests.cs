181cf222f3b1f518e52600674deadf84410c3b9d
Rick Cuthbertson <rick.cuthbertson@yappy.com>
2023-03-28 15:09:15 +0100

    handle scanning and return the totals.  added basic tests

--- checkout-kata-tests/CheckoutTests.cs
+++ checkout-kata-tests/CheckoutTests.cs
@@ -33,5 +33,48 @@ namespace checkout_kata_tests
 
             act.Should().Throw<MissingDataException>().WithMessage($"Product with Sku '{badSku}' not found");
         }
+
+        [Fact]
+        public void ScanningProductGivesCorrectPrice()
+        {
+            var sku = "A";
+            var unitPrice = 50;
+
+            var sut = new Checkout(new ProductBuilder().WithSku(sku).WithUnitPrice(unitPrice).Build());
+
+            sut.Scan(sku);
+            sut.GetTotalPrice().Should().Be(unitPrice);
+        }
+
+        [Fact]
+        public void ScanningSameProductMultipleTimesGivesCorrectPrice()
+        {
+            var sku = "A";
+            var unitPrice = 50;
+
+            var sut = new Checkout(new ProductBuilder().WithSku(sku).WithUnitPrice(unitPrice).Build());
+
+            sut.Scan(sku);
+            sut.Scan(sku);
+
+            sut.GetTotalPrice().Should().Be(unitPrice * 2);
+        }
+
+        [Fact]
+        public void ScanningMultipleItemsGivesCorrectPrice()
+        {
+            var sku1 = "A";
+            var unitPrice1 = 50;
+            var sku2 = "B";
+            var unitPrice2 = 30;
+
+            var sut = new Checkout(new ProductBuilder().WithSku(sku1).WithUnitPrice(unitPrice1).Build(),
+                new ProductBuilder().WithSku(sku2).WithUnitPrice(unitPrice2).Build());
+
+            sut.Scan(sku1);
+            sut.Scan(sku2);
+
+            sut.GetTotalPrice().Should().Be(unitPrice1 + unitPrice2);
+        }
     }
 }
--- checkout-kata/Checkout.cs
+++ checkout-kata/Checkout.cs
@@ -1,5 +1,6 @@
 ﻿namespace checkout_kata
 {
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
     using checkout_kata_contracts;
@@ -7,10 +8,12 @@
     public class Checkout : ICheckout
     {
         private readonly IReadOnlyDictionary<string, Product> products;
+        private readonly ConcurrentBag<Product> items;
 
         public Checkout(params Product[] products)
         {
             this.products = products.ToDictionary(k => k.Sku, v => v);
+            this.items = new ConcurrentBag<Product>();
         }
 
         public void Scan(string item)
@@ -19,11 +22,13 @@
             {
                 throw new MissingDataException(item);
             }
+
+            this.items.Add(this.products[item]);
         }
 
         public int GetTotalPrice()
         {
-            throw new System.NotImplementedException();
+            return this.items.Sum(i => i.UnitPrice);
         }
     }
 }
